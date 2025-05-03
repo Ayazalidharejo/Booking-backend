fb5e312a9adaa45eedc96cf6af583dd6f0d46502
Ayazalidharejo <ayazali.xwave@gmail.com>
2025-05-03 17:06:07 +0500

    sql

--- index.js
+++ index.js
@@ -965,6 +965,6 @@ app.post('/api/events', auth, async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
